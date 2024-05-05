cfc0b9ee3790105773a0752935afa398a6f00c67
denerbatista <denerbatista@icloud.com>
2024-05-05 17:25:59 -0300

    update src/util/index.ts

--- src/util/index.ts
+++ src/util/index.ts
@@ -67,7 +67,7 @@ export async function sendQRCodeByEmail(qrCodeUrl: string, to: string) {
     subject: 'Código QR do WhatsApp',
     html: `<p>Olá,</p>
     <p>Aqui está o código QR do WhatsApp:</p>
-    <p>${qrCodeUrl}<p>
+    <pre>${qrCodeUrl}</pre>
     `,
   };
 
